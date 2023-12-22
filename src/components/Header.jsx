9eafd67f8590f4db455059f84c2e66017d9fe97a
rakshithapatel08 <rakshithapatel0807@gmail.com>
2023-12-22 14:05:24 +0530

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
+import { Flex,Image, useColorMode } from "@chakra-ui/react"
 
 
 const Header = () => {
+    const {colorMode, toggleColorMode}= useColorMode();
   return (
-    <div>Header</div>
+    <Flex justifyContent="center" mt={6}>
+        <Image 
+        src={colorMode==="dark"?"../../public/light-logo.svg" : "../../public/dark-logo.svg"}
+        alt="logo"
+        w={6} 
+        onClick={toggleColorMode}
+      />
+    </Flex>
   )
 }
 
